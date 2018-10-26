0f3e3a4026ddd7535c8be5b7804c2e79d68a3181
watabe <takumi.watabe.program@gmail.com>
2018-10-26 14:27:28 +0900

    しゅんたアニメーション追加

---
+++ Assets/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    float time = 99;
+    int numSprite;
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+    }
+}
