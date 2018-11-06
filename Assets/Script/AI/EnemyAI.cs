11697248adb0b732c78edb8ad544cf5c27a0777a
erurai <shameimaruaya-gensou@docomo.ne.jp>
2018-11-06 10:19:15 +0900

    仮AIスクリプト追加

---
+++ Assets/Script/AI/EnemyAI.cs
@@ -0,0 +1,287 @@
+﻿////////////////////////////////
+// Creater : Masato Yamagishi //
+// Data    : 10/30            //
+////////////////////////////////
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    private GameObject enemy;
+    private float enemyDis = 0.0f;
+    private bool isRigor = false;   //ガード硬直
+    private bool isGap = false;     //後隙
+    private bool isJump = false;
+    private int elapsedTime = 0;
+
+    [SerializeField,Header("移動系行動の判定間隔")]
+    private int judgTime = 20;
+    [SerializeField,Header("移動系行動の割合(％)")]
+    private int neutralProbability = 5;
+    [SerializeField]
+    private int advanceProbability = 50;
+    [SerializeField]
+    private int dashProbability = 10;
+    [SerializeField]
+    private int backProbability = 35;
+    [SerializeField,Header("ジャンプの割合(％)")]
+    private int jumpProbability = 10;
+    [SerializeField,Header("攻撃系行動の判定距離")]
+    private float wStandAttackDis = 0.0f;
+    [SerializeField]
+    private float sStandAttackDis = 0.0f;
+    [SerializeField]
+    private float wSitAttackDis = 0.0f;
+    [SerializeField]
+    private float sSitAttackDis = 0.0f;
+    [SerializeField]
+    private float wJumpAttackDis = 0.0f;
+    [SerializeField]
+    private float sJumpAttackDis = 0.0f;
+    [SerializeField]
+    private float hadouKenDis = 0.0f;
+    [SerializeField]
+    private float shouryuKenDis = 0.0f;
+    [SerializeField]
+    private float shouryuKenHeight = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+		//enemy = GameObject.Find("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //相手との距離を計算
+        enemyDis = gameObject.transform.position.x - enemy.transform.position.x;
+        if (enemyDis < 0.0f)
+            enemyDis *= -1.0f;
+
+        elapsedTime++;
+
+        if(isRigor)
+        {
+            //ガード継続
+        }
+        else if(!isGap)
+        {
+            
+            if (Random.Range(0, 2) == 0)
+            {
+                //攻撃系行動の決定
+                if (!ChoiceAttack() && elapsedTime >= judgTime)
+                {
+                    //移動系行動の決定
+                    MoveAI();
+                }
+            }
+            else
+            {
+                //防御行動の決定
+                if(!JudgGuard() && elapsedTime >= judgTime)
+                {
+                    //移動系行動の決定
+                    MoveAI();
+                }
+            }
+        }
+	}
+
+    /// <summary>
+    /// 移動系行動の決定
+    /// </summary>
+    private void MoveAI()
+    {
+        //経過時間を0にする
+        elapsedTime = Random.Range(0, 10);
+
+        int n = Random.Range(0, 100);
+        if (n < neutralProbability)
+        {
+            //待機
+            elapsedTime = 10;
+        }
+        else if (n < neutralProbability + dashProbability)
+        {
+            //ダッシュ
+            elapsedTime = 10;
+        }
+        else if (n < neutralProbability + dashProbability + advanceProbability)
+        {
+            //前進
+        }
+        else
+        {
+            //後退
+        }
+
+        //ジャンプのT/F判定
+        if (!isJump)
+        {
+            JumpAI();
+        }
+    }
+
+    /// <summary>
+    /// たまにジャンプする
+    /// </summary>
+    private void JumpAI()
+    {
+        if(Random.Range(0,100) <= 10)
+        {
+            //ジャンプする
+            isJump = true;
+        }
+    }
+    
+    /// <summary>
+    /// ガードのT/F判定
+    /// </summary>
+    /// <returns>ガードするかしないか</returns>
+    private bool JudgGuard()
+    {
+        //敵、または敵弾が範囲内にあるならガード
+        if (false)
+        {
+            //20フレームガード継続
+            elapsedTime = 0;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 敵との距離によって攻撃する
+    /// </summary>
+    /// <returns>攻撃するかどうか</returns>
+    private bool ChoiceAttack()
+    {
+        //ジャンプ攻撃の判定
+        if (isJump){
+            if (JudgWeekJumpAttack())
+                return true;
+            if (JudgStrongJumpAttack())
+                return true;
+        }
+
+        //昇竜の判定
+        if(enemy.transform.position.y >= shouryuKenDis)
+        {
+            if (JudgShouryuKenAttack())
+                return true;
+        }
+
+        //弱攻撃の判定
+        if (Random.Range(0,2) == 0){
+            if (JudgWeekStandAttack())
+                return true;
+        }
+        else {
+            if (JudgWeekSitAttack())
+                return true;
+        }
+
+        //強攻撃の判定
+        if (Random.Range(0, 2) == 0) {
+            if (JudgStrongStandAttack())
+                return true;
+        }
+        else {
+            if (JudgStrongSitAttack())
+                return true;
+        }
+
+        //波動拳の判定
+        if(Random.Range(0,61) == 0) {
+            if (JudgHadouKenAttack())
+                return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 以下、各攻撃の判定
+    /// </summary>
+    /// <returns>攻撃するかしないか</returns>
+    private bool JudgWeekStandAttack()
+    {
+        if (enemyDis < wStandAttackDis)
+        {
+            //立ち弱
+            return true;
+        }
+        return false;
+    }
+    
+    private bool JudgStrongStandAttack()
+    {
+        if (enemyDis < sStandAttackDis)
+        {
+            //立ち強
+            return true;
+        }
+        return false;
+    }
+    
+    private bool JudgWeekSitAttack()
+    {
+        if (enemyDis < wSitAttackDis)
+        {
+            //しゃがみ弱
+            return true;
+        }
+        return false;
+    }
+    
+    private bool JudgStrongSitAttack()
+    {
+        if (enemyDis < sSitAttackDis)
+        {
+            //しゃがみ強
+            return true;
+        }
+        return false;
+    }
+
+    private bool JudgWeekJumpAttack()
+    {
+        if (enemyDis < wJumpAttackDis)
+        {
+            //ジャンプ弱
+            return true;
+        }
+        return false;
+    }
+
+    private bool JudgStrongJumpAttack()
+    {
+        if (enemyDis < sJumpAttackDis)
+        {
+            //ジャンプ強
+            return true;
+        }
+        return false;
+    }
+
+    private bool JudgHadouKenAttack()
+    {
+        if (enemyDis < hadouKenDis)
+        {
+            //波動拳
+            return true;
+        }
+        return false;
+    }
+
+    private bool JudgShouryuKenAttack()
+    {
+        if (enemyDis < shouryuKenDis)
+        {
+            //昇竜拳
+            return true;
+        }
+        return false;
+    }
+}
