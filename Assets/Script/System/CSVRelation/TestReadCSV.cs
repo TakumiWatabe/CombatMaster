dd831002dc56f98b4c551706dc72c9a16c98d463
erurai <shameimaruaya-gensou@docomo.ne.jp>
2018-11-16 18:42:15 +0900

    CSVデータの読み込み

---
+++ Assets/Script/System/CSVRelation/TestReadCSV.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestReadCSV : MonoBehaviour {
+
+    private ReadCSV rc;
+    private Dictionary<string, ReadCSV.CharaData> aoiData;
+
+	// Use this for initialization
+	void Start () {
+        rc = GetComponent<ReadCSV>();
+
+        aoiData = rc.readCSVData("AOI");
+
+        Debug.Log(aoiData.Count);
+        rc.WriteCharaDatas(aoiData, aoiData.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
