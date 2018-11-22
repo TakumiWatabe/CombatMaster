649cc617ce9e68064b73e5d551c535bfd33c9262
watabe <takumi.watabe.program@gmail.com>
2018-11-22 11:20:52 +0900

    セレクトシーンUI実装

---
+++ Assets/Script/System/IconEvent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class IconEvent : MonoBehaviour
+{
+    //　このスロットのアイテム名
+    [SerializeField]
+    private string itemName;
+    GameObject frame1p;
+    GameObject frame2p;
+    GameObject frame2p2;
+    private Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        frame1p = GameObject.Find("1Pframe");
+        frame2p = GameObject.Find("2Pframe");
+        frame2p2 = GameObject.Find("2Pframe2");
+    }
+
+    //　マウスアイコンが自分のアイコン上に入った時
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        CheckEvent(col);
+    }
+
+    //　マウスアイコンが自分のアイコン上にいる間
+    void OnTriggerStay2D(Collider2D col)
+    {
+        CheckEvent(col);
+    }
+
+    void CheckEvent(Collider2D col)
+    {
+        //　アイコンを検知する
+        if (col.tag == "icon1")
+        {
+            frame1p.transform.localPosition = col.gameObject.GetComponent<CharacterSelect>().GetFramePos(itemName);
+        }
+        else if(col.tag=="icon2")
+        {
+            frame2p.transform.localPosition = col.gameObject.GetComponent<CharacterSelect>().GetFramePos(itemName);
+            frame2p2.transform.localPosition = col.gameObject.GetComponent<CharacterSelect>().GetFramePos(itemName);
+        }
+    }
+}
---
+++ Assets/Script/System/SceneTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+    SceneManagement scene;
+    FadeScript fade;
+	// Use this for initialization
+	void Start () {
+        scene = this.GetComponent<SceneManagement>();
+        fade = GameObject.Find("FadePanel").GetComponent<FadeScript>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (SceneManager.GetActiveScene().name == "TitleScene")
+        {
+            if(Input.anyKeyDown)
+            {
+                fade.FadeOutFlag();
+                //scene.SceneChange("play");
+            }
+        }
+
+    }
+}
