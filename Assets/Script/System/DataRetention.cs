771a4dc03c4b12d9882d68296cdd3e1b82eb108e
erurai <shameimaruaya-gensou@docomo.ne.jp>
2018-11-22 23:37:41 +0900

    プレイメニューシーンの作成

---
+++ Assets/Script/System/DataRetention.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataRetention : MonoBehaviour {
+
+    //ゲームモードの列挙型
+    public enum GameMode
+    {
+        PvP,
+        PvC,
+        SIZE
+    }
+
+    private int mode;
+
+    //propaty
+    public int Mode
+    {
+        get
+        {
+            return mode;
+        }
+        set
+        {
+            mode = value;
+        }
+    }
+
+}
